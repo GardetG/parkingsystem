c79df4b1a915eb1a1dfbdb1b10299d179c8ce40e
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-10-29 17:02:30 +0200

    Merge BugFix_FareCalculator feature
    -Update log4j2 configuration
    -Update pom.xml to generate Jacoco, surefire and spotbugs report
    -Update project and Replace Date by LocalDateTime from java.time API
    -Add AssertJ library
    -Complete and reorganize FareCalculatorService unit tests
    -Implement calculateDuration
    -Fix calculateFare

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -1,39 +1,58 @@
 package com.parkit.parkingsystem.util;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Scanner;
-
+/**
+ * Utility Class handling the console input from the user.
+ * 
+ *
+ */
 public class InputReaderUtil {
 
-    private static Scanner scan = new Scanner(System.in);
-    private static final Logger logger = LogManager.getLogger("InputReaderUtil");
+  private static Scanner scan = new Scanner(System.in, "UTF-8");
+  private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
-    public int readSelection() {
-        try {
-            int input = Integer.parseInt(scan.nextLine());
-            return input;
-        }catch(Exception e){
-            logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter valid number for proceeding further");
-            return -1;
-        }
+  /**
+   * Read the menu option provide by the user and return an associated integer or
+   * -1 in case of an invalid input.
+   *
+
+   * @return integer associated to user's input.
+   */
+  public int readSelection() {
+    try {
+      return Integer.parseInt(scan.nextLine());
+    } catch (Exception e) {
+      logger.error("Error while reading user input from Shell", e);
+      System.out.println("Error reading input. Please enter valid number for proceeding further");
+      return -1;
     }
+  }
 
-    public String readVehicleRegistrationNumber() throws Exception {
-        try {
-            String vehicleRegNumber= scan.nextLine();
-            if(vehicleRegNumber == null || vehicleRegNumber.trim().length()==0) {
-                throw new IllegalArgumentException("Invalid input provided");
-            }
-            return vehicleRegNumber;
-        }catch(Exception e){
-            logger.error("Error while reading user input from Shell", e);
-            System.out.println("Error reading input. Please enter a valid string for vehicle registration number");
-            throw e;
-        }
+  /**
+   * Read the vehicle registration number provide by the user and return a string
+   * or throws an exception in case of an invalid input.
+   * 
+
+   * @return String associated to vehicle registration number.
+   * @throws IllegalArgumentException
+   */
+  public String readVehicleRegistrationNumber() throws IllegalArgumentException {
+    try {
+      String vehicleRegNumber = scan.nextLine();
+      if (vehicleRegNumber == null || vehicleRegNumber.trim().length() == 0) {
+        throw new IllegalArgumentException("Invalid input provided");
+      }
+      return vehicleRegNumber;
+    } catch (Exception e) {
+      logger.error("Error while reading user input from Shell", e);
+      System.out.println(
+          "Error reading input. Please enter a valid string for vehicle registration number");
+      throw e;
     }
-
+  }
 
 }
--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -1,127 +1,227 @@
 package com.parkit.parkingsystem;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+class FareCalculatorServiceTest {
 
-import java.util.Date;
+  private static FareCalculatorService fareCalculatorService;
+  private Ticket ticket;
+  private LocalDateTime inTime;
+  private LocalDateTime outTime;
 
-public class FareCalculatorServiceTest {
+  @BeforeAll
+  private static void setUp() {
+    fareCalculatorService = new FareCalculatorService();
+  }
 
-    private static FareCalculatorService fareCalculatorService;
-    private Ticket ticket;
-
-    @BeforeAll
-    private static void setUp() {
-        fareCalculatorService = new FareCalculatorService();
-    }
+  @DisplayName("calculateFare method test")
+  @Nested
+  class CalculateFareTest {
 
     @BeforeEach
     private void setUpPerTest() {
-        ticket = new Ticket();
+      ticket = new Ticket();
+      outTime = LocalDateTime.now();
+      ticket.setOutTime(outTime);
     }
 
+    @DisplayName("Calculate bike fare")
+    @ParameterizedTest(name = "Bike fare for {0} minutes should equal to {1} * "
+            + Fare.BIKE_RATE_PER_HOUR)
+    @CsvSource({ "0,0", "45,0.75", "60,1","90,1.5" ,"1440,24" })
+    void calculateFareBike(int numberOfMinutes, double expectedMultiplier) {
+      // GIVEN
+      inTime = outTime.minusMinutes(numberOfMinutes);
+      ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
+
+      ticket.setInTime(inTime);
+      ticket.setParkingSpot(parkingSpot);
+      double expectedFare = expectedMultiplier * Fare.BIKE_RATE_PER_HOUR;
+
+      // WHEN
+      fareCalculatorService.calculateFare(ticket);
+
+      // THEN
+      assertThat(ticket.getPrice()).isEqualTo(expectedFare);
+    }
+
+    @DisplayName("Calculate car fare")
+    @ParameterizedTest(name = "Car fare for {0} minutes should equal to {1} *"
+            + Fare.CAR_RATE_PER_HOUR)
+    @CsvSource({ "0,0", "45,0.75", "60,1","90,1.5", "1440,24" })
+    void calculateFareCar(int numberOfMinutes, double expectedMultiplier) {
+      // GIVEN
+      inTime = outTime.minusMinutes(numberOfMinutes);
+      ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+
+      ticket.setInTime(inTime);
+      ticket.setParkingSpot(parkingSpot);
+      double expectedFare = expectedMultiplier * Fare.CAR_RATE_PER_HOUR;
+
+      // WHEN
+      fareCalculatorService.calculateFare(ticket);
+
+      // THEN
+      assertThat(ticket.getPrice()).isEqualTo(expectedFare);
+    }
+
+    @DisplayName("Calculate fare with future in time")
+    @ParameterizedTest(name = "Calculate {0} fare with future in time should throws exception")
+    @CsvSource({ "Bike", "Car" })
+    void calculateFareWithFutureInTime(String type) {
+      // GIVEN
+      inTime = outTime.plusMinutes(60);
+      ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.valueOf(type.toUpperCase()), false);
+
+      ticket.setInTime(inTime);
+      ticket.setParkingSpot(parkingSpot);
+
+      // WHEN
+      Throwable thrown = catchThrowable(() -> fareCalculatorService.calculateFare(ticket));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("Out time provided is incorrect:");
+    }
+
+    @DisplayName("Calculate fare with null out time")
+    @ParameterizedTest(name = "Calculate {0} fare with null out time should throws exception")
+    @CsvSource({ "Bike", "Car" })
+    void calculateFareWithNullOutTime(String type) {
+      // GIVEN
+      ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.valueOf(type.toUpperCase()), false);
+
+      ticket.setInTime(LocalDateTime.now());
+      ticket.setOutTime(null);
+      ticket.setParkingSpot(parkingSpot);
+
+      // WHEN
+      Throwable thrown = catchThrowable(() -> fareCalculatorService.calculateFare(ticket));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("Out time provided is incorrect:null");
+    }
+
+    @DisplayName("Calculate fare with null in time")
+    @ParameterizedTest(name = "Calculate {0} fare with null in time should throws exception")
+    @CsvSource({ "Bike", "Car" })
+    void calculateFareWithNullInTime(String type) {
+      // GIVEN
+      ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.valueOf(type.toUpperCase()), false);
+
+      ticket.setInTime(null);
+      ticket.setOutTime(LocalDateTime.now());
+      ticket.setParkingSpot(parkingSpot);
+
+      // WHEN
+      Throwable thrown = catchThrowable(() -> fareCalculatorService.calculateFare(ticket));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("In time provided is incorrect:null");
+    }
+
+    @DisplayName("Calculate fare for null parking type should throws exception")
     @Test
-    public void calculateFareCar(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+    void calculateFareNullType() {
+      // GIVEN
+      inTime = outTime.minusMinutes(60);
+      ParkingSpot parkingSpot = new ParkingSpot(1, null, false);
 
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        fareCalculatorService.calculateFare(ticket);
-        assertEquals(ticket.getPrice(), Fare.CAR_RATE_PER_HOUR);
+      ticket.setInTime(inTime);
+      ticket.setParkingSpot(parkingSpot);
+
+      // WHEN
+      Throwable thrown = catchThrowable(() -> fareCalculatorService.calculateFare(ticket));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(NullPointerException.class);
+    }
+  }
+
+  @DisplayName("calculateDuration method test")
+  @Nested
+  class CalculateDurationTest {
+
+    @DisplayName("calculate duration")
+    @ParameterizedTest(name = "Duration for {0} minutes should equal to {1}")
+    @CsvSource({ "0,0", "15,0.25", "30,0.5", "45,0.75", "60,1", "90,1.5", "1440,24" })
+    void calculateDuration(int numberOfMinutes, double expectedDuration) {
+      // GIVEN
+      outTime = LocalDateTime.now();
+      inTime = outTime.minusMinutes(numberOfMinutes);
+
+      // WHEN
+      double actualDuration = fareCalculatorService.calculateDuration(inTime, outTime);
+
+      // THEN
+      assertThat(actualDuration).isEqualTo(expectedDuration);
     }
 
+    @DisplayName("calculate duration futur in time should throws an exception")
     @Test
-    public void calculateFareBike(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+    void calculateDurationWithFutureInTime() {
+      // GIVEN
+      outTime = LocalDateTime.now();
+      inTime = outTime.plusMinutes(60);
 
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        fareCalculatorService.calculateFare(ticket);
-        assertEquals(ticket.getPrice(), Fare.BIKE_RATE_PER_HOUR);
+      // WHEN
+      Throwable thrown = catchThrowable(
+              () -> fareCalculatorService.calculateDuration(inTime, outTime));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("Out time provided is incorrect:");
     }
 
+    @DisplayName("calculate duration when out time is null should throws an exception")
     @Test
-    public void calculateFareUnkownType(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, null,false);
+    void calculateDurationWithOutTimeNull() {
+      // GIVEN
+      outTime = null;
+      inTime = LocalDateTime.now();
 
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        assertThrows(NullPointerException.class, () -> fareCalculatorService.calculateFare(ticket));
+      // WHEN
+      Throwable thrown = catchThrowable(
+              () -> fareCalculatorService.calculateDuration(inTime, outTime));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("Out time provided is incorrect:null");
     }
 
+    @DisplayName("calculate duration when in time is null should throws an exception")
     @Test
-    public void calculateFareBikeWithFutureInTime(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() + (  60 * 60 * 1000) );
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+    void calculateDurationWithInTimeNull() {
+      // GIVEN
+      outTime = LocalDateTime.now();
+      inTime = null;
 
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        assertThrows(IllegalArgumentException.class, () -> fareCalculatorService.calculateFare(ticket));
+      // WHEN
+      Throwable thrown = catchThrowable(
+              () -> fareCalculatorService.calculateDuration(inTime, outTime));
+
+      // THEN
+      assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+              .hasMessageContaining("In time provided is incorrect:null");
     }
-
-    @Test
-    public void calculateFareBikeWithLessThanOneHourParkingTime(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  45 * 60 * 1000) );//45 minutes parking time should give 3/4th parking fare
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
-
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        fareCalculatorService.calculateFare(ticket);
-        assertEquals((0.75 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice() );
-    }
-
-    @Test
-    public void calculateFareCarWithLessThanOneHourParkingTime(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  45 * 60 * 1000) );//45 minutes parking time should give 3/4th parking fare
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        fareCalculatorService.calculateFare(ticket);
-        assertEquals( (0.75 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
-    }
-
-    @Test
-    public void calculateFareCarWithMoreThanADayParkingTime(){
-        Date inTime = new Date();
-        inTime.setTime( System.currentTimeMillis() - (  24 * 60 * 60 * 1000) );//24 hours parking time should give 24 * parking fare per hour
-        Date outTime = new Date();
-        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-
-        ticket.setInTime(inTime);
-        ticket.setOutTime(outTime);
-        ticket.setParkingSpot(parkingSpot);
-        fareCalculatorService.calculateFare(ticket);
-        assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
-    }
-
+  }
 }
